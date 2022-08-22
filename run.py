b935385dfc0de4380c5f9e930ad3b998317322e3
devmarcstorm <43650740+devmarcstorm@users.noreply.github.com>
2022-08-22 15:28:34 +0200

    Fix errors in run.py
    	- Ä,Ö, and Ü are now displayed correctly (UTF-8 encoding).
    	- Questionnaires can now be digitized one after the other.
    	- Answers were not saved before. Questions were written to the answer files.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 DATA_DIRECTORY = "data/"
@@ -13,40 +15,47 @@ def get_questions() -> list:
 
 
 if __name__ == "__main__":
-    questions = get_questions()
-    answers = []
-
-    new_questions = False
-    if questions == []:
-        new_questions = True
-
-    current_question_index = 0
     while True:
-        current_question = ""
-        try:
-            current_question = questions[current_question_index]
-            print(current_question)
-        except IndexError:
-            print("Neue Frage definieren: ('exit' um beantwortung zu beenden)")
-            current_question = input()
-            if current_question == "exit":
-                break
-            questions.append(current_question)
-            print("Antwort:")
+        questions = get_questions()
+        answers = []
 
-        answers.append(input())
-        current_question_index += 1
+        new_questions = False
+        if questions == []:
+            new_questions = True
 
-    print("Fragebogen-ID:")
-    fragebogen_id = input()
+        current_question_index = 0
+        while True:
+            current_question = ""
+            try:
+                current_question = questions[current_question_index]
+                print(current_question)
+            except IndexError:
+                print("Neue Frage definieren: ('exit' um beantwortung zu beenden)")
+                current_question = input()
+                if current_question == "exit":
+                    break
+                questions.append(current_question)
+                print("Antwort:")
 
-    if not os.path.exists(DATA_DIRECTORY):
-        os.mkdir(DATA_DIRECTORY)
+            answers.append(input())
+            current_question_index += 1
 
-    with open(QUESTIONS_FILE_PATH, "w+") as f:
-        for question in questions:
-            f.write(question + "\n")
+        print("Fragebogen-ID:")
+        fragebogen_id = input()
 
-    with open(DATA_DIRECTORY + fragebogen_id + ".txt", "w+") as f:
-        for question in questions:
-            f.write(question + "\n")
+        if not os.path.exists(DATA_DIRECTORY):
+            os.mkdir(DATA_DIRECTORY)
+
+        with open(QUESTIONS_FILE_PATH, "w+") as f:
+            for question in questions:
+                f.write(question + "\n")
+
+        with open(DATA_DIRECTORY + fragebogen_id + ".txt", "w+") as f:
+            for answer in answers:
+                f.write(answer + "\n")
+
+        print("Weiteren Fragebogen ausfüllen? (j / N)")
+        if input() == "j":
+            pass
+        else:
+            break
